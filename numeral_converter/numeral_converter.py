befe19380a35d4a55e692e040737190324ceeb7d
tv <lytvynenkotv@gmail.com>
2023-01-31 12:51:08 +0200

    temp fixed data path

--- numeral_converter/numeral_converter.py
+++ numeral_converter/numeral_converter.py
@@ -13,7 +13,7 @@ from .utils import combinations
 
 NumberItem = namedtuple("NumberItem", "value order scale")
 
-DATA_PATH = Path("/Users/tatiana/PycharmProjects/numeral-converter/data")
+DATA_PATH = Path("")
 
 NUMERAL_TREE: Dict[str, Any] = dict()
 NUMERAL_DATA: Dict[str, pd.DataFrame] = dict()
